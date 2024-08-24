83f51f2511ea8986f80a7ef86d94771baa7017a4
Probir Sarkar <probirsarkar855@gmail.com>
2024-08-24 15:49:50 +0530

    cors update

--- configs/env.ts
+++ configs/env.ts
@@ -1,7 +1,7 @@
 const env = {
   TELEGRAM_URL: Deno.env.get("TELEGRAM_URL"),
   TELEGREM_CHAT_ID: Deno.env.get("TELEGREM_CHAT_ID"),
-  CORS: ["https://probirsarkar.com","https://probir.dev"]
+  CORS: Deno.env.get("CORS")?.split(",") || []
 };
 
 export default env;
